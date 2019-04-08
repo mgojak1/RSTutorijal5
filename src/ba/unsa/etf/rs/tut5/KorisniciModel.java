882036831b6695ed0e8356a469bd0e56b62a2213
tsijercic1 <tsijercic1@etf.unsa.ba>
2019-04-08 10:54:32 +0200

    Napravljena KorisniciModel klasa

---
+++ src/ba/unsa/etf/rs/tut5/KorisniciModel.java
@@ -0,0 +1,35 @@
+package ba.unsa.etf.rs.tut5;
+
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+public class KorisniciModel {
+    private ObservableList<Korisnik> korisnici;
+    private SimpleObjectProperty<Korisnik> trenutniKorisnik;
+
+    public KorisniciModel() {
+        this.korisnici = FXCollections.observableArrayList();
+        this.trenutniKorisnik = new SimpleObjectProperty<>();
+    }
+
+    public ObservableList<Korisnik> getKorisnici() {
+        return korisnici;
+    }
+
+    public void setKorisnici(ObservableList<Korisnik> korisnici) {
+        this.korisnici = korisnici;
+    }
+
+    public Korisnik getTrenutniKorisnik() {
+        return trenutniKorisnik.get();
+    }
+
+    public SimpleObjectProperty<Korisnik> trenutniKorisnikProperty() {
+        return trenutniKorisnik;
+    }
+
+    public void setTrenutniKorisnik(Korisnik trenutniKorisnik) {
+        this.trenutniKorisnik.set(trenutniKorisnik);
+    }
+}
